b56d8a0ec6a79556d9fad2ee78212cc13b620ac9
blackshovon <blackshovon@gmail.com>
2021-04-19 14:03:21 +0600

    first commit

---
+++ src/components/Bookings/Bookings.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
+
+const Bookings = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [bookings, setBookings] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/bookings?email='+loggedInUser.email,{
+            method: 'GET',
+            headers: {
+                'content-type' : 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => setBookings(data))
+    }, [])
+    return (
+        <div>
+            <h3>you have : {bookings.length} bookings</h3>
+            {
+                bookings.map(book=> <li>{book.name} from: {(new Date(book.checkIn).toDateString('dd/MM/yyyy'))} to: {(new Date(book.checkOut).toDateString('dd/MM/yyyy'))}</li>)
+            }
+        </div>
+    );
+};
+
+export default Bookings;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmAWXsalG2tErN-m0GfQLV0m-c_7oqtbw",
+  authDomain: "burz-al-arab-5ae71.firebaseapp.com",
+  projectId: "burz-al-arab-5ae71",
+  storageBucket: "burz-al-arab-5ae71.appspot.com",
+  messagingSenderId: "1092310597337",
+  appId: "1:1092310597337:web:acb07aa12f58370191c8c8"
+};
+  export default firebaseConfig;
